e877ffaef623f788224db7e63793c3e960889650
Alvaro Perez Rodriguez <261786878+AlvPerez192@users.noreply.github.com>
2026-05-04 04:14:55 +0200

    Change VM size from Standard_B1s to Standard_B1ms

--- terraform/azure-pilot/main.tf
+++ terraform/azure-pilot/main.tf
@@ -222,7 +222,7 @@ resource "azurerm_linux_virtual_machine" "docker" {
   name                = "tfg-pilot-docker-vm"
   resource_group_name = data.azurerm_resource_group.dr.name
   location            = data.azurerm_resource_group.dr.location
-  size                = "Standard_B1s"
+  size                = "Standard_B1ms"
 
   admin_username                  = "azureuser"
   admin_password                  = var.vm_admin_password
