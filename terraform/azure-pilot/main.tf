5e4d0865aac700e40eca51bd7d4b8ecc13405ef3
Alvaro Perez Rodriguez <261786878+AlvPerez192@users.noreply.github.com>
2026-05-04 03:43:55 +0200

    Remove location from DR resource group
    Removed location attribute from the resource group data block.

--- terraform/azure-pilot/main.tf
+++ terraform/azure-pilot/main.tf
@@ -69,7 +69,6 @@ variable "vm_admin_password" {
 # -----------------------------------------------------------------------------
 data "azurerm_resource_group" "dr" {
   name     = "tfg-final-spain-rg"
-  location = "Spain Central"
 
   tags = {
     Project = "TFG-MultiCloud"
